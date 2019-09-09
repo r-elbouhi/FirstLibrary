0706c7bdd001f30f643b392af612d22d5c09669b
r_elbouhi <rami.elbouhi@gmail.com>
2019-09-09 12:41:58 +0200

    rename xml file

--- app/src/main/java/com/example/libtest/LibTestActivity.kt
+++ app/src/main/java/com/example/libtest/LibTestActivity.kt
@@ -16,6 +16,6 @@ class LibTestActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_lib_test)
     }
 }
